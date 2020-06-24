615bb8cb6daf6318d5200d27009ac10ded7683d9
Anthony Belcastro <belcastt@live.com>
2020-06-24 12:55:55 -0700

    Adding skeleton workup outline adding in the need for a function for keeping score, and writing a highs score.  I also need to find time keeping in the next version update.

---
+++ triviascript.js
@@ -0,0 +1,23 @@
+const startBtn = document.getElementById('start=btn')
+
+startBtn.addEventlistener('click', startQuiz)
+
+function startQuiz(params) {
+
+}
+
+function moveNextQ(params) {
+
+}
+
+function pickAnswer(params) {
+
+}
+
+function kpscore(params) {
+
+}
+
+function writeHighScore() {
+
+}
