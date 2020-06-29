66df009fc8ce82267cca1f01ff9c5e3c2027d4b6
Anthony Belcastro <belcastt@live.com>
2020-06-29 15:06:44 -0700

    finishing start button actions in java, start, hide, move to question, and moveNextQ()

--- triviascript.js
+++ triviascript.js
@@ -1,16 +1,22 @@
 const startBtn = document.getElementById('start-btn');
-
+const qBucket = document.getElementById('question-container')
 startBtn.addEventListener('click', startQuiz);
 
 function startQuiz() {
-    console.log('Begin the game')
+    console.log('let the TRIVIA BEGIN.')
+    startBtn.classList.add('hide')
+    qBucket.classList.remove('hide')
+    moveNextQ()
 }
+// function timeKeeper(params) {
+
+// }
 
 // function timeKeeper(params) {
 
 // }
 
-// function moveNextQ(params) {
+// function moveNextQ() {
 
 // }
 
